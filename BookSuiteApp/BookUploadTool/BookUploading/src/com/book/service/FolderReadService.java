88533810e9f8e4c73c1450e4b285355d8b9061a9
sunip123 <sujit.singh@hotmail.com>
2015-07-13 23:32:21 +0530

    read folder class added

---
+++ BookSuiteApp/BookUploadTool/BookUploading/src/com/book/service/FolderReadService.java
@@ -0,0 +1,28 @@
+package com.book.service;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class FolderReadService {
+	public ArrayList<String> getMedia() {
+
+		File folder = new File("E:/Media");
+		File[] listOfFiles = folder.listFiles();
+
+		ArrayList<String> list = new ArrayList<String>();
+
+		for (int i = 0; i < listOfFiles.length; i++) {
+			if (listOfFiles[i].isFile())
+
+			{
+				list.add(listOfFiles[i].getName());
+
+			}
+
+		}
+		return (list);
+	}
+
+	
+}
