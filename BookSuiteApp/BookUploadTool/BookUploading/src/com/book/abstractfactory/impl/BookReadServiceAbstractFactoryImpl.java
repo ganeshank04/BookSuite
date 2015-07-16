ff2aab493f2f5f082b55eb97a118e2662e0776bb
ganeshank <ganeshank04@gmail.com>
2015-07-16 19:45:14 +0530

    Abstract Design Pattern

---
+++ BookSuiteApp/BookUploadTool/BookUploading/src/com/book/abstractfactory/impl/BookReadServiceAbstractFactoryImpl.java
@@ -0,0 +1,7 @@
+package com.book.abstractfactory.impl;
+
+import AbstractFactory.ServiceAbstractFactory;
+
+public class BookReadServiceAbstractFactoryImpl extends ServiceAbstractFactory {
+
+}
---
+++ BookSuiteApp/BookUploadTool/BookUploading/src/com/book/factory/producer/FactoryProducer.java
@@ -0,0 +1,10 @@
+package com.book.factory.producer;
+
+import AbstractFactory.ServiceAbstractFactory;
+
+public class FactoryProducer {
+	public static ServiceAbstractFactory getFactory(String choice){
+		   
+	      return null;
+	   }
+}
